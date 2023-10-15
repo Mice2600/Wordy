397d8ee12e91af1e92440fb63d25612724c3457d
Lacon <58324951+Mice2600@users.noreply.github.com>
2023-10-15 23:39:00 +0500

    sd

--- Assets/Study/BuildTheBox/BuildTheBox.cs
+++ Assets/Study/BuildTheBox/BuildTheBox.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using SystemBox;
+using SystemBox.Simpls;
 using TMPro;
 using UnityEngine;
 
@@ -15,6 +16,7 @@ public class BuildTheBox : MonoBehaviour
         
     }
     Dictionary<Vector2Int, char> Places;
+    public TList<TList<Vector2Int>> Giz;
     [Button]
     void Generate()
     {
@@ -36,6 +38,10 @@ public class BuildTheBox : MonoBehaviour
             List<(Vector2Int, char)> values = Acomadate(C.EnglishSource, i);
             values.ForEach(d => Places.Add(d.Item1, d.Item2));
         });
+
+
+        Giz = Divorce(new List<Vector2Int>(Places.Keys));
+
         CreatG();
 
         List<(Vector2Int, char)> Acomadate(string Word, int Y) 
@@ -58,8 +64,139 @@ public class BuildTheBox : MonoBehaviour
         {
             return false;
         }
+        //---------------------------------------------------------------
+        
+
+    }
+
+    TList<TList<Vector2Int>> Divorce(TList<Vector2Int> Positions) 
+    {
+
+        Vector2Int SaverMax = new Vector2Int(-99, -99);
+        Vector2Int SaverMin = new Vector2Int(99, 99);
+        for (int i = 0; i < Positions.Count; i++)
+        {
+            if (SaverMax.x < Positions[i].x) SaverMax.x = Positions[i].x;
+            if (SaverMax.y < Positions[i].y) SaverMax.y = Positions[i].y;
+            if (Positions[i].x < SaverMin.x) SaverMin.x = Positions[i].x;
+            if (Positions[i].y < SaverMin.y) SaverMin.y = Positions[i].y;
+        }
+        Vector2Int UR = SaverMax;
+        Vector2Int DL = SaverMin;
+        Vector2Int UL = new Vector2Int(SaverMin.x, SaverMax.y);
+        Vector2Int DR = new Vector2Int(SaverMax.x, SaverMin.y);
+
+        List<Vector2Int> ConerPatterns = new List<Vector2Int>() { new(2,2), new(2, 3), new(3, 2) };
+        
+        //List<Vector2Int> OtherPatterns = new List<Vector2Int>() {new(2, 1), new(1, 2)};
+        TList<TList<Vector2Int>> Divorced = new TList<TList<Vector2Int>>();
+        DivorceConner(ConerPatterns.RandomItem(), UR, new(-1,-1));
+        DivorceConner(ConerPatterns.RandomItem(), UL, new(1,-1));
+        DivorceConner(ConerPatterns.RandomItem(), DR, new(-1,1));
+        DivorceConner(ConerPatterns.RandomItem(), DL, new(1,1));
+
+        void DivorceConner(Vector2Int Size, Vector2Int Coner, Vector2Int MoveSide)
+        {
+            List<Vector2Int> NeedPoses = new List<Vector2Int>();
+            for (int y = 0; y < Size.y; y++)
+                for (int x = 0; x < Size.x; x++) 
+                    NeedPoses.Add(Coner + new Vector2Int((MoveSide.x >0)?x : -x, (MoveSide.y > 0) ? y : -y));
+            TList<Vector2Int> ResoltPoses = new List<Vector2Int>();
+            NeedPoses.ForEach(d => ResoltPoses.AddIf(d, Positions.Contains(d)));
+            ResoltPoses.ForEach(a => Positions.Remove(a));
+            Divorced.Add(ResoltPoses);
+        }
 
 
+
+
+
+
+        List<List<List<Vector2Int>>> AllDevorses = new List<List<List<Vector2Int>>>();
+
+        List<Vector2Int> OtherBigPatterns = new() {  new(2, 3), new(2, 2) };
+        List<Vector2Int> OtherPatterns = new() { new(5, 2), new(4, 2), new(5, 1),new(4, 1),new(1, 3),new(2, 1),new(1, 1)};
+        OtherBigPatterns.Mix().ForEach(s => OtherPatterns.AddTo(0, s));
+
+
+
+
+
+
+
+        List<(List<List<Vector2Int>> AllDevorses, List<Vector2Int> LeftPoses)> Resolts = new();
+        for (int i = 0; i < OtherPatterns.Count; i++)
+        {
+            Resolts = new();
+            FindAcomadation(Positions, new List<List<Vector2Int>>(), OtherPatterns[i]);
+           
+            (List<List<Vector2Int>> AllDevorses, List<Vector2Int> LeftPoses)? Ch1 = null;
+            Resolts.ForEach(a => { if (Ch1 == null || Ch1.Value.LeftPoses.Count > a.LeftPoses.Count) Ch1 = a; });
+            if (Ch1 != null) 
+            {
+                Ch1.Value.AllDevorses.ForEach(a => Divorced.Add(a));
+                Ch1.Value.AllDevorses.ForEach(d => d.ForEach(qq => Positions.Remove(qq)));
+            }
+        }
+
+        void FindAcomadation(List<Vector2Int> LeftPoses, List<List<Vector2Int>> Devorses, Vector2Int Patterns) 
+        {
+            foreach (var Box in LeftPoses)
+            {
+                List<Vector2Int> OtherPatterns = new List<Vector2Int>() { Patterns, new Vector2Int(Patterns.y, Patterns.x) }.Mix();
+                foreach (var Pattern in OtherPatterns)
+                {
+                    if (CanAcomadate(Pattern, Box))
+                    {
+                        List<Vector2Int> NewLeftPoses = Divorce(Pattern, Box, out List<Vector2Int> EDDS);
+                        List<List<Vector2Int>> NewDevorses = new List<List<Vector2Int>>(Devorses) { EDDS };
+                        if (NewLeftPoses.Count == 0)
+                        {
+                            //Debug.Log("dd");
+                            Resolts.Add((NewDevorses, NewLeftPoses));
+                            return;
+                        }
+                        FindAcomadation(NewLeftPoses, NewDevorses, Pattern);
+                    }
+                    else if (LeftPoses.Count != Positions.Count) 
+                    {
+                        if (Resolts.Count > 0)
+                        {
+                            if (Resolts.Last().AllDevorses.Last()[0] != Devorses.Last()[0]) 
+                            {
+                                Resolts.Add((Devorses, LeftPoses));
+                            }
+                        }
+                        else Resolts.Add((Devorses, LeftPoses));
+                        
+                    }
+                }
+            }
+            
+            bool CanAcomadate(Vector2Int Size, Vector2Int Coner)
+            {
+                for (int y = 0; y < Size.y; y++)
+                    for (int x = 0; x < Size.x; x++) 
+                        if (!LeftPoses.Contains(Coner + new Vector2Int(x, y))) 
+                            return false;
+                return true;
+            }
+            List<Vector2Int> Divorce(Vector2Int Size, Vector2Int Coner, out List<Vector2Int> Devorsed) 
+            {
+                Devorsed = new List<Vector2Int>();
+                for (int y = 0; y < Size.y; y++)
+                    for (int x = 0; x < Size.x; x++)
+                        Devorsed.Add(Coner + new Vector2Int(x, y));
+                TList<Vector2Int> ResoltPoses = new List<Vector2Int>(LeftPoses);
+                Devorsed.ForEach(a => ResoltPoses.Remove(a));
+                return ResoltPoses;
+            }
+
+        }
+
+
+
+        return Divorced;
     }
     public GameObject PPCC;
     public GameObject Parrent;
@@ -71,6 +208,16 @@ public class BuildTheBox : MonoBehaviour
             dd.transform.localPosition = new Vector3(K.x, K.y, 0);
             dd.GetComponentInChildren<TMP_Text>().text = Places[K].ToString();
         });
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Giz.ForEach((g, i) => 
+        {
+            Gizmos.color = new TList<Color>() 
+            {Color.red, Color.white, Color.blue, Color.green, Color.gray, Color.black, Color.cyan, Color.magenta}[i, ListGetType.Loop]; 
+            g.ForEach(d => Gizmos.DrawCube(Parrent.transform.position + new Vector3(d.x, d.y, 0) * .6f, new Vector3(.5f, .5f, .5f))); });
     }
 
 }
